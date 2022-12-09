2a5e3ea5e4aa7700675e1b36f38e23387a8d0ccd
charles-ram <ram.charles429@gmail.com>
2022-12-09 00:23:44 -0600

    Updated javascript. Created prompts and alerts to check if user input value is between 8 and 128 characters. Still need to add an additional conditional statement for decimals.

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,18 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  //var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+  passwordLength = prompt("How many characters would you like your character to be? Enter a number between 8 and 128.");
+  // Still need to prevent decimals from getting through.
+  if (passwordLength >= 8 && passwordLength <=128) {
+    alert("You have chosen " + passwordLength + " characters.")
+    console.log("You have chosen " + passwordLength + " characters.")
+  } else {
+    alert("Your answer is not a valid number. Try Again.")
+    console.log("Your answer is not a valid number. Try Again.")
+  }
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
